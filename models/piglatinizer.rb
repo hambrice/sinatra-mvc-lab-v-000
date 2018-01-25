75ee5424e30f44f81e2c0b724099df43d8dbcd02
Brad Hambrice <hambrice@uab.edu>
2018-01-25 02:34:10 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 class PigLatinizer
-  attr_accessor :text 
+  attr_accessor :text
 
   def initialize(text)
     @text = text
@@ -10,22 +10,13 @@ class PigLatinizer
   def create
     final = []
     array = self.text.split(" ")
-    array.each do |word|
-      arr=word.split("")
-      arr.each do |l|
-        if l.match(/[aeiou]/)
-          arr2 = arr.join.split(l)
-          arr3 = []
-          arr3[0] = arr2[1]
-          arr3[1] = arr2[0]
-          new_word = l + arr3.join + "ay"
-          final << new_word
-        end
-      end
-    end
-      binding.pry
-        final.join(" ")
-    end
+    array.collect do |word|
+      word = word.split(/([aeiou].*)/)
+      word[1] += "ay"
+      word[2] = word[0]
+      word.shift.join
+    end.join
+  end
 
 
 
